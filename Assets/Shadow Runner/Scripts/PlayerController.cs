55efba3bb59f92f7a022d12157ad0a5ee4318988
Gopi467420 <gopiofficialca20@gmail.com>
2025-01-13 21:37:56 -0500

    Scripts

--- Assets/Shadow Runner/Scripts/PlayerController.cs
+++ Assets/Shadow Runner/Scripts/PlayerController.cs
@@ -27,8 +27,12 @@ public class PlayerController : MonoBehaviour
     private bool isHidden = false;
     private RectTransform _healthbar;
 
+    public static PlayerController instance;
+
     void Start()
     {
+
+        instance = this;    
         // Initialize health and states
         health = 5;
         _initialposition = transform.position;
@@ -37,27 +41,15 @@ public class PlayerController : MonoBehaviour
         _currentstate = PlayerState.visible;
 
         _healthbar = GameObject.FindWithTag("Healthbar")?.GetComponent<RectTransform>();
-        if (_healthbar == null)
-        {
-            Debug.LogError("Healthbar not found! Ensure the Healthbar is tagged correctly.");
-        }
+        
 
         _playerrigidbody = GetComponent<Rigidbody>();
-        if (_playerrigidbody == null)
-        {
-            Debug.LogError("Player Rigidbody is not assigned!");
-        }
+       
 
         _touchManager = FindAnyObjectByType<TouchManager>();
-        if (_touchManager == null)
-        {
-            Debug.LogError("TouchManager not found in the scene!");
-        }
+        
 
-        if (_throwdestinationrigidbody == null)
-        {
-            Debug.LogError("Throw Destination Rigidbody is not assigned! Please assign it in the Inspector.");
-        }
+       
 
         
     }
@@ -76,86 +68,42 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    private void FixedUpdate()
+  
+
+    public void MovePlayer(Vector2 _moveinput)
     {
-        if (_touchManager.GetInputControls().Player.enabled)
-        {
-            MovePlayer();
-            // Subscribe to player movement input actions
-            _touchManager.GetInputControls().Player.PlayerMovement.performed += OnMovementPerformed;
-            _touchManager.GetInputControls().Player.PlayerMovement.canceled += OnMovementCanceled;
-
-            // Unsubscribe from throw movement
-            _touchManager.GetInputControls().Throw.ProjectileSetdestination.performed -= OnMovementPerformed;
-            _touchManager.GetInputControls().Throw.ProjectileSetdestination.canceled -= OnMovementCanceled;
-        }
-        else if (_touchManager.GetInputControls().Throw.enabled)
-        {
-            AimProjectile();
-            ThrowProjectile();
-            
-
-            // Subscribe to throw movement
-            _touchManager.GetInputControls().Throw.ProjectileSetdestination.performed += OnMovementPerformed;
-            _touchManager.GetInputControls().Throw.ProjectileSetdestination.canceled += OnMovementCanceled;
-
-            // Unsubscribe from player movement input actions
-            _touchManager.GetInputControls().Player.PlayerMovement.performed -= OnMovementPerformed;
-            _touchManager.GetInputControls().Player.PlayerMovement.canceled -= OnMovementCanceled;
-        }
-    }
-
-    private void MovePlayer()
-    {
-        if (_playerrigidbody == null || _throwdestinationrigidbody == null) return;
-
+        
         // Movement logic for player
-        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y).normalized * moveSpeed;
-        _playerrigidbody.linearVelocity = new Vector3(movement.x, _playerrigidbody.linearVelocity.y, movement.z);
+        Vector3 movement = new Vector3(_moveinput.x, 0f, _moveinput.y).normalized * moveSpeed;
 
-        // Hide the destination movement
-        AimProjectile();
+        transform.Translate(movement * Time.fixedDeltaTime);
+        
+
         _throwdestinationrigidbody.gameObject.SetActive(false);
     }
 
-    public void AimProjectile()
+    public void AimProjectile(Vector2  _moveinput)
     {
-        
         // Movement logic
-        Vector3 movement = new Vector3(moveInput.x, 0f, moveInput.y).normalized * moveSpeed;
+        Vector3 movement = new Vector3(_moveinput.x, 0f, _moveinput.y).normalized * moveSpeed;
         _throwdestinationrigidbody.linearVelocity = new Vector3(movement.x, _playerrigidbody.linearVelocity.y, movement.z);
-        _throwdestinationrigidbody.gameObject.SetActive(true);
-        //SwipeDetection.instance.SetCurrentSwipeDirection(SwipeDetection.SwipeDirection.None);
-
        
     }
 
-    public void ThrowProjectile()
-    {
-       // if (SwipeDetection.instance.DetectSwipe() == SwipeDetection.SwipeDirection.Up)
-        {
-            Debug.Log("Throw action triggered!");
-            //_touchManager.SwitchActionMap(_touchManager.GetInputControls().Player);
-        }
-    }
+   public void ShowAim() { _throwdestinationrigidbody.gameObject.SetActive(true); }
 
-    // Capture movement input
-    private void OnMovementPerformed(InputAction.CallbackContext context)
-    {
-        // Get the movement input from the action context
-        moveInput = context.ReadValue<Vector2>();
-    }
+   public void HideAim() { _throwdestinationrigidbody.gameObject.SetActive(false); }
+    
 
-    // Reset movement input when canceled
-    private void OnMovementCanceled(InputAction.CallbackContext context)
-    {
-        moveInput = Vector2.zero;
-    }
+   
+    private void OnMovementPerformed(InputAction.CallbackContext context) { moveInput = context.ReadValue<Vector2>(); }   
+    private void OnMovementCanceled(InputAction.CallbackContext context) { moveInput = Vector2.zero; }
+   
 
     void Hidden()
     {
         // Switch to hidden state
-        originalstate.SetActive(false);
+        originalstate.SetActive(false);      
         hiddenstate.SetActive(true);
     }
 
@@ -166,31 +114,10 @@ public class PlayerController : MonoBehaviour
         hiddenstate.SetActive(false);
     }
 
-    public void SetCurrentState(PlayerState state)
-    {
-        _currentstate = state;
-    }
-
-    public bool GetIsHidden()
-    {
-        return isHidden;
-    }
-
-    public void SetIsHidden(bool hidden)
-    {
-        isHidden = hidden;
-    }
-
-    public Vector3 GetInitialPosition()
-    {
-        return _initialposition;
-    }
-
-    public int GetHealth()
-    {
-        return health;
-    }
-
+    public void SetCurrentState(PlayerState state) { _currentstate = state; }
+    
+    public int GetHealth() { return health; }
+    
     public void TakeDamage(int damageAmount)
     {
         health -= damageAmount;
--- Assets/Shadow Runner/Scripts/SwipeDetection.cs
+++ Assets/Shadow Runner/Scripts/SwipeDetection.cs
@@ -1,27 +1,170 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class SwipeDetection : MonoBehaviour
 {
-   private TouchManager _touchManager;
+    private TouchManager _touchManager;
+    private Vector2 _swipestartposition, _swipeendposition, _touchposition;
+    private float _swipetime;
+    public float _desiredswipedistance;
+
+    public enum SwipeDirection { None, Left, Right, Up, Down };
+    private SwipeDirection _currentswipedirection;
+
+    public enum Quadrants { None, Left, Right, Up, Down };
+    private Quadrants _currentquadrant;
+
+    public static SwipeDetection instance;
+
+    private float screenWidth;
+    private float screenHeight;
+
+    private void Awake()
+    {
+        // Singleton pattern enforcement
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+    }
 
     private void Start()
     {
-
         _touchManager = gameObject.GetComponent<TouchManager>();
+
+        _swipetime = 0;
+        _currentswipedirection = SwipeDirection.None;
+        _currentquadrant = Quadrants.None;
+
+        screenWidth = Screen.width;
+        screenHeight = Screen.height;
+
+
         
+       
+
+
     }
     private void Update()
     {
+        TouchManager.instance.GetInputControls().Throw.PrimaryContact.performed += ctx => StartTouchPrimary(ctx);
+        TouchManager.instance.GetInputControls().Throw.PrimaryContact.canceled += ctx => EndTouchPrimary(ctx);
+        TouchManager.instance.GetInputControls().Throw.PrimaryPosition.performed += ctx => GetTouchposition(ctx);
 
-        _touchManager.GetInputControls().Throw.PrimaryContact.performed += ctx => StartTouchPrimary(ctx);
+      
+
+    }
+
+   private void GetTouchposition(InputAction.CallbackContext callbackContext)
+    {
+        _touchposition = callbackContext.ReadValue<Vector2>();
+       
+
+    }
+
+    public void StartTouchPrimary(InputAction.CallbackContext callbackContext)
+    {
+        _swipestartposition = _touchposition;
+       
+       
+    }
+
+    public void EndTouchPrimary(InputAction.CallbackContext callbackContext)
+    {
+        _swipeendposition = _touchposition;
+        DetectSwipe();
+
+        Debug.Log("Start" + DetectSwipe());
+
+
+    }
+
+    public Quadrants CheckVectorposition(Vector2 positiontocheck)
+    {
+        // Check for the Right quadrant
+        if (positiontocheck.x > screenWidth / 2 && positiontocheck.x < screenWidth &&
+            positiontocheck.y > 0 && positiontocheck.y < screenHeight)
+        {
+            _currentquadrant = Quadrants.Right;
+        }
+        // Check for the Left quadrant
+        else if (positiontocheck.x > 0 && positiontocheck.x < screenWidth / 2 &&
+                 positiontocheck.y > 0 && positiontocheck.y < screenHeight)
+        {
+            _currentquadrant = Quadrants.Left;
+        }
+        // Check for the Up quadrant
+        else if (positiontocheck.y > 0 && positiontocheck.y < screenHeight / 2 &&
+                 positiontocheck.x > 0 && positiontocheck.x < screenWidth)
+        {
+            _currentquadrant = Quadrants.Up;
+        }
+        // Check for the Down quadrant
+        else if (positiontocheck.y > screenHeight / 2 && positiontocheck.y < screenHeight &&
+                 positiontocheck.x > 0 && positiontocheck.x < screenWidth)
+        {
+            _currentquadrant = Quadrants.Down;
+        }
+
+        return _currentquadrant;
+    }
+
+    public bool CheckSwipeQuadrants(Quadrants _quadranttocheck)
+    {
+        if (CheckVectorposition(_swipestartposition) == _quadranttocheck &&
+            CheckVectorposition(_swipeendposition) == _quadranttocheck)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void ResetSwipeParametres()
+    {
+        _swipeendposition = _swipestartposition = Vector2.zero;
+        _currentswipedirection = SwipeDirection.None;
+        _currentquadrant = Quadrants.None;
         
     }
 
-    private void StartTouchPrimary(InputAction.CallbackContext callbackContext)
+
+    // Method to detect swipe direction
+    public SwipeDirection DetectSwipe()
     {
+        if (Vector2.Distance(_swipestartposition, _swipeendposition) >= _desiredswipedistance)
+        {
+            // Calculate the normalized direction vector
+            Vector2 swipeDirection = (_swipeendposition - _swipestartposition).normalized;
 
+            // Determine the primary direction of the swipe
+            if (Mathf.Abs(swipeDirection.x) > Mathf.Abs(swipeDirection.y))
+            {
+                // Horizontal swipe
+                if (swipeDirection.x > 0)
+                    _currentswipedirection = SwipeDirection.Right;
+                else
+                    _currentswipedirection = SwipeDirection.Left;
+            }
+            else
+            {
+                // Vertical swipe
+                if (swipeDirection.y > 0)
+                    _currentswipedirection = SwipeDirection.Up;
+                else
+                    _currentswipedirection = SwipeDirection.Down;
+            }
 
+            Debug.Log("Swipe Direction: " + _currentswipedirection);
+        }
+        else
+        {
+            _currentswipedirection = SwipeDirection.None;
+        }
+
+        return _currentswipedirection;
     }
-
 }
